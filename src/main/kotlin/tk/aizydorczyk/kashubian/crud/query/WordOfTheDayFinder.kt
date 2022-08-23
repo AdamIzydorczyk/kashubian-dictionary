c349e6068a74458ee9b3b788efc6e2e35743a8e1
Adam Izydorczyk <adamflorianizydorczyk@gmail.com>
2022-08-23 10:52:54 +0200

    word of the day

--- src/main/kotlin/tk/aizydorczyk/kashubian/crud/query/WordOfTheDayFinder.kt
+++ src/main/kotlin/tk/aizydorczyk/kashubian/crud/query/WordOfTheDayFinder.kt
@@ -1,5 +1,7 @@
 package tk.aizydorczyk.kashubian.crud.query
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import tk.aizydorczyk.kashubian.crud.domain.KashubianEntryRepository
 import java.time.Clock.systemUTC
@@ -9,11 +11,12 @@ import kotlin.random.Random
 
 @Component
 class WordOfTheDayFinder(val kashubianEntryRepository: KashubianEntryRepository) {
-
+    val logger: Logger = LoggerFactory.getLogger(javaClass)
     val cache = AtomicReference(CachedWordOfTheDay(0L, 0L))
 
     fun find(): Long {
-        val seed = now(systemUTC()).toEpochDay()
+        val currentDay = now(systemUTC())
+        val seed = currentDay.toEpochDay()
 
         if (cache.get().seed == seed) {
             return cache.get().wordOfTheDayId
@@ -23,7 +26,7 @@ class WordOfTheDayFinder(val kashubianEntryRepository: KashubianEntryRepository)
             .random(Random(seed))
 
         cache.getAndSet(CachedWordOfTheDay(seed = seed, wordOfTheDayId = wordOfTheDayId))
-
+        logger.info("Word of day for $currentDay selected and cached")
         return wordOfTheDayId
     }
 
