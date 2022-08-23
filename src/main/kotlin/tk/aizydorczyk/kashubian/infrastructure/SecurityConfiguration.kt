dfcdfa4e08c04c58472cfd4b298685f730646e4f
Adam Izydorczyk <adamflorianizydorczyk@gmail.com>
2022-08-23 23:08:09 +0200

    cors configuration

--- src/main/kotlin/tk/aizydorczyk/kashubian/infrastructure/SecurityConfiguration.kt
+++ src/main/kotlin/tk/aizydorczyk/kashubian/infrastructure/SecurityConfiguration.kt
@@ -10,20 +10,32 @@ import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.channel.ChannelProcessingFilter
+import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.OncePerRequestFilter
 import tk.aizydorczyk.kashubian.crud.model.value.AnnotationConstants.Companion.FILE_PATH
 import tk.aizydorczyk.kashubian.crud.model.value.AnnotationConstants.Companion.KASHUBIAN_ENTRY_PATH
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration(private val corsFilter: CorsFilter) {
 
     @Bean
     fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
         return httpSecurity.also {
-            it.antMatcher("/**")
+            it.addFilterBefore(corsFilter, ChannelProcessingFilter::class.java)
+                .antMatcher("/**")
                 .csrf().disable()
-                .cors().disable()
+                .cors()
+                .configurationSource(corsConfig())
+                .disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -40,6 +52,8 @@ class SecurityConfiguration {
                 .permitAll()
                 .antMatchers(HttpMethod.POST, "/graphql")
                 .permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -49,6 +63,19 @@ class SecurityConfiguration {
         }.build()
     }
 
+    private fun corsConfig(): CorsConfigurationSource {
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**",
+                    CorsConfiguration().apply {
+                        allowedOrigins = listOf("*")
+                        allowedMethods = listOf("*")
+                        allowedHeaders = listOf("*")
+                        allowCredentials = true
+                    }
+            )
+        }
+    }
+
     @Bean
     fun userDetailsService(): UserDetailsService = User.builder()
         .username("admin")
@@ -56,3 +83,24 @@ class SecurityConfiguration {
         .roles("ADMIN")
         .build().let { InMemoryUserDetailsManager(it) }
 }
+
+@Component
+class CorsFilter : OncePerRequestFilter() {
+    override fun doFilterInternal(request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain) {
+        response.apply {
+            setHeader("Access-Control-Allow-Origin", "*")
+            setHeader("Access-Control-Allow-Methods", "*")
+            setHeader("Access-Control-Allow-Headers", "*")
+        }
+
+        if ("OPTIONS" == request.method) {
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            filterChain.doFilter(request, response)
+        }
+    }
+
+}
+
